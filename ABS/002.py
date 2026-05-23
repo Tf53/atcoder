cfcd2bd7c90d2e1cd37ad75c63231ad196862649
Tf53 <tomofuji4900@gmail.com>
2026-05-23 18:05:17 +0900

    002 commit

---
+++ ABS/002.py
@@ -0,0 +1,24 @@
+from itertools import product
+
+if __name__ == "__main__":
+  N = int(input())
+
+  def check(S):
+    count = 0
+    for c in S:
+      if c == '(':
+        count += 1
+      else:
+        count -= 1
+
+      if count < 0:
+        return False
+    
+    return (count == 0)
+
+
+  for S in product(['(', ')'], repeat=N):
+    if check(S):
+      print(*S, sep='')
+
+
