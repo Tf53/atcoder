b991942989b3a026a5f7a7d3b2871bb2041d0fb1
Tf53 <tomofuji4900@gmail.com>
2026-05-23 23:12:43 +0900

    d.py commit

--- ABC/459/d.py
+++ ABC/459/d.py
@@ -1,10 +1,41 @@
 if __name__  == "__main__":
   T = int(input())
 
+  def solve():
+    s = input()
+    n = len(s)
+
+    cnt = [0] * 26
+
+    for c in s:
+      cnt[ord(c) - ord('a')] += 1
+    
+    for i in range(26):
+      if cnt[i] > (n + 1) // 2:
+        print("No")
+        return
+      
+    ans = []
+    pre = -1
+    while True:
+      dev = 0
+      idx = -1
+      for i in range(26):
+        if pre == i:
+          continue
+        if dev < cnt[i]:
+          idx = i
+          dev = cnt[i]
+      if idx == -1:
+        break
+      ans.append(chr(ord('a') + idx))
+      cnt[idx] -= 1
+      pre = idx
+      
+    print("Yes")
+    print(*ans, sep='')
+
+
   for _ in range(T):
-    S = input()
-    count = [0 for _ in range(26)]
-    for s in S:
-      count[ord(s) - ord('a')] += 1
-    count.sort()
+    solve()
     
